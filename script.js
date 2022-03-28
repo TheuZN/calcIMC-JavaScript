ce2752010552ba3a56ef9d3714f81ae527d85227
Mateus Melo <mateus.gon.melo@gmail.com>
2022-03-28 20:43:35 -0300

    atualizacao

--- script.js
+++ script.js
@@ -11,5 +11,30 @@ function calculo() {
 
     let total = (peso / (altura*altura)).toFixed(2);
 
-    resultado.innerHTML = `<p> O resultado é ${total}</p>`;
+    if (total >= 0.00 && total <= 18.5) {
+        resultado.innerHTML = `<p> Seu IMC é ${total} - Você está abaixo do peso</p>`;
+    }
+
+    else if (total >= 18.6 && total <= 24.99) {
+        resultado.innerHTML = `<p> Seu IMC é ${total} - Você está com o peso normal</p>`;
+        }
+
+    else if (total >= 25 && total <= 29.99) {
+        resultado.innerHTML = `<p> Seu IMC é ${total} - Você está com sobrepeso</p>`;
+    }
+
+    else if (total >= 30 && total <= 34.99) {
+        resultado.innerHTML = `<p> Seu IMC é ${total} - Você está em estado de obesidade I</p>`;
+    }
+
+    else if (total >= 35 && total <= 39.99) {
+        resultado.innerHTML = `<p> Seu IMC é ${total} - Você está em estado de obesidade II</p>`;
+    }
+
+    else if (total >= 40) {
+        resultado.innerHTML = `<p> Seu IMC é ${total} - <br> Você está em estado de obesidade III</p>`;
+    }
+
+    else {
+        resultado.innerHTML = `<p>Por favor, verifique as informações.</p>`;    }
 }
